8a0f5d6d802d0696ca4390c9524509263bd7dc83
jaketrent <trent.jake@gmail.com>
2017-06-19 05:58:08 -0600

    put letters on buttons

--- src/app/buttons/index.js
+++ src/app/buttons/index.js
@@ -19,15 +19,15 @@ const Button = props =>
       className={props.css.press}
       onclick={_ => props.addContainer(props.color)}
     >
-      A
+      {props.letter}
     </button>
   </div>
 
 const Buttons = props =>
   <div className={props.css.buttons}>
-    <Button {...props} color="red" />
-    <Button {...props} color="yellow" />
-    <Button {...props} color="blue" />
+    <Button {...props} color="red" letter="Q" />
+    <Button {...props} color="yellow" letter="W" />
+    <Button {...props} color="blue" letter="E" />
   </div>
 
 export default connect(null, mapPropsToDispatch, store)(styleable(css)(Buttons))
