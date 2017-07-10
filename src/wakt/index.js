e376df93b8fbc9f5398bc261ba49ef0b8a6ba3ee
jaketrent <trent.jake@gmail.com>
2017-07-10 07:39:20 -0600

    extract helper function to make isUpdate clearer

--- src/wakt/index.js
+++ src/wakt/index.js
@@ -32,8 +32,10 @@ function createElement(nodeName, props, ...children) {
 const renders = {}
 let evtDelegator = null
 
+const isUpdate = el => !!renders[el]
+
 function render(vnode, el) {
-  if (renders[el]) {
+  if (isUpdate(el)) {
     const { currentVNode, rootNode } = renders[el]
     renders[el] = {
       currentVNode: vnode,
